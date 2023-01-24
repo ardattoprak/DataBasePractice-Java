8dc6fd5efd98a9db4129989d85ef62e56fefba34
ardatoprak <ardattoprak@gmail.com>
2023-01-24 21:32:28 +0300

    Connected to database

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,24 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Main {
-    public static void main(String[] args)
-    {
+
+    static  String userName = "root";
+    static  String password = "12345678";
+    static  String dbUrl = "jdbc:mysql://localhost:3306/world";
+
+    public static void main(String[] args) throws SQLException {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(dbUrl, userName, password);
+            System.out.println("Bağlantı oluştu");
+        } catch (SQLException exception) {
+            System.out.println(exception.getMessage());
+        }
+        finally {
+            connection.close();
+        }
 
     }
 }
