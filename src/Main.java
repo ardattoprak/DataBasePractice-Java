d04b1e951820705caae02bc1000d94d4822ca92b
ardatoprak <ardattoprak@gmail.com>
2023-01-24 22:47:15 +0300

    insert codes wrote

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,33 @@ public class Main {
     public static void main(String[] args) throws SQLException {
         Connection connection = null;
         DbHelper helper = new DbHelper();
+        PreparedStatement statement = null;
+        ResultSet resultSet;
+        try {
+
+            connection = helper.getConnection();
+            String sql = "insert into city (Name,CountryCode,District,Population) values('?','?','?',?)";
+            System.out.println("Bağlantı oluştu");
+            statement = connection.prepareStatement(sql);
+            statement.setString(1,"Düzce");
+            statement.setString(2 ,"TUR");
+            statement.setString(3,"Turkey");
+            statement.setInt(4,70000);
+            int result = statement.executeUpdate();
+            System.out.println("Kayıt eklendi");
+
+        } catch (SQLException exception) {
+            helper.showErrorMessage(exception);
+        }
+        finally {
+            statement.close();
+            connection.close();
+        }
+    }
+
+    public static void selectDemo()throws SQLException {
+        Connection connection = null;
+        DbHelper helper = new DbHelper();
         Statement statement = null;
         ResultSet resultSet;
         try {
@@ -15,11 +42,11 @@ public class Main {
             resultSet = statement.executeQuery("select Code,Name,Continent,Region from country ");
             ArrayList<Country> countries = new ArrayList<>();
             while (resultSet.next()) {
-               countries.add(new Country(
-                       resultSet.getString("Code"),
-                       resultSet.getString("Name"),
-                       resultSet.getString("Continent"),
-                       resultSet.getString("Region")));
+                countries.add(new Country(
+                        resultSet.getString("Code"),
+                        resultSet.getString("Name"),
+                        resultSet.getString("Continent"),
+                        resultSet.getString("Region")));
             }
             System.out.println(countries.size());
 
